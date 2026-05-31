58b2ed8230a5042e38c7a2509a4bbcf7962c4f66
N.Akshay <akshayreddynareddy386@gmail.com>
2026-05-31 20:26:08 +0530

    messageinput

---
+++ frontend/src/components/MessageInput.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Image} from 'lucide-react';
+const MessageInput = () => {
+    const [imagePreview, setImagePreview] = useState(null);
+  return (
+
+    <div className="p-4 w-full">
+      {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MessageInput
