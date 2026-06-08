5a12d2fc2fbcbf76d1044b8256334e6a3eafc023
N.Akshay <akshayreddynareddy386@gmail.com>
2026-06-08 21:28:18 +0530

    messageinput handlefunctions

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
-import {Image} from 'lucide-react';
+import {Image, X} from 'lucide-react';
 const MessageInput = () => {
 
+  const [text, setText] = useState("");
   const [imagePreview, setImagePreview] = useState(null);
   const fileInputRef = useRef(null);
+  const { sendMessage } = useChatStore();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (!file.type.startsWith("image/")) {
+      toast.error("Please select an image file");
+      return;
+    }
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
   const removeImage = () => {
     setImagePreview(null);
     if (fileInputRef.current) fileInputRef.current.value = "";
+  };
+
+  const handleSendMessage =(e) => {
+    e.preventDefault();
+    if(!text.trim() && !imagePreview) return;
+    try {
+      await SendMessage({
+        text:text.trim(),
+        image:imagePreview,
+      });
+      setText("");
+      setImagePreview(null);
+      if (fileInputRef.current) fileInputRef.current.value = "";
+    }
+    catch (error) {
+      console.error("Error sending message:", error);
+    }
   };
   return (
 
@@ -30,6 +63,16 @@ const MessageInput = () => {
           </div>
         </div>
       )}
+      <form onSubmit={handleSendMessage} className="flex items-center gap-2">
+          <div className="flex-1 flex gap-2">
+            <input type="text" className="w-full input input-bordered rounded-lg input-sm sm:input-md" 
+            placeholder="Type your message..."
+            value={text} onChange={(e) => setText(e.target.value)} />
+            <input type="file" accept="image/*" ref={fileInputRef} 
+            onChange={handleImageChange} className="hidden" 
+            />
+          </div>
+      </form>
     </div>
   )
 }
